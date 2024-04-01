5e31445533e22c1f9a1a5830a8b2b23a9cd24d5e
Sid-1819 <shirdhankarsiddhesh@atharvacoe.ac.in>
2024-04-01 16:12:47 +0530

    CartItem Complete

--- src/Rent/ItemCart.java
+++ src/Rent/ItemCart.java
@@ -1,5 +1,29 @@
 package Rent;
 
-public class ItemCart {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ItemCart {
+private List<RentalItem> CartList;
+
+public ItemCart(){
+   this.CartList = new ArrayList<>();
+}
+
+public void addItem(RentalItem item){
+    CartList.add(item);
+}
+    public void updateItem(int index, RentalItem item){
+    CartList.set(index,item);
+    }
+
+    public void deleteItem(int index){
+    CartList.remove(index);
+    }
+
+    public void showItem(){
+        for (RentalItem item: CartList) {
+            System.out.println(item.toString());
+        }
+    }
 }
